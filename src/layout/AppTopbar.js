fc09905b30fc1fcfc1dbad8cb7af01319789f0b9
thdtjdgml415 <thdtjdgml415@naver.com>
2023-03-21 19:44:24 +0900

    이게 맞냐고,,

--- src/layout/AppTopbar.js
+++ src/layout/AppTopbar.js
@@ -1,69 +1,87 @@
-import getConfig from 'next/config';
-import Link from 'next/link';
-import Router, { useRouter } from 'next/router';
-import { classNames } from 'primereact/utils';
-import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
-import { LayoutContext } from './context/layoutcontext';
+import getConfig from "next/config";
+import Link from "next/link";
+import Router, { useRouter } from "next/router";
+import { classNames } from "primereact/utils";
+import React, {
+  forwardRef,
+  useContext,
+  useImperativeHandle,
+  useRef,
+} from "react";
+import { LayoutContext } from "./context/layoutcontext";
 
-import { Select } from 'antd';
+import { Select } from "antd";
 
 const AppTopbar = forwardRef((props, ref) => {
-    const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
-    const menubuttonRef = useRef(null);
-    const topbarmenuRef = useRef(null);
-    const topbarmenubuttonRef = useRef(null);
-    const contextPath = getConfig().publicRuntimeConfig.contextPath;
+  const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } =
+    useContext(LayoutContext);
+  const menubuttonRef = useRef(null);
+  const topbarmenuRef = useRef(null);
+  const topbarmenubuttonRef = useRef(null);
+  const contextPath = getConfig().publicRuntimeConfig.contextPath;
 
-    useImperativeHandle(ref, () => ({
-        menubutton: menubuttonRef.current,
-        topbarmenu: topbarmenuRef.current,
-        topbarmenubutton: topbarmenubuttonRef.current
-    }));
+  useImperativeHandle(ref, () => ({
+    menubutton: menubuttonRef.current,
+    topbarmenu: topbarmenuRef.current,
+    topbarmenubutton: topbarmenubuttonRef.current,
+  }));
 
+  const onChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  const onSearch = (value) => {
+    console.log("search:", value);
+  };
 
-
-    const onChange = (value) => {
-        console.log(`selected ${value}`);
-    };
-    const onSearch = (value) => {
-        console.log('search:', value);
-    };
-
-
-    return (
-        <div className="layout-topbar">
-            <Link href="/">
-                <div className="layout-topbar-logo">
-                    <>
-                        <img src={`${contextPath}/layout/images/logo-main.svg`} width="177.22px" height={'40px'} widt={'true'} alt="logo" />
-                        <span className='ir'>pims</span>
-                    </>
-                </div>
-            </Link>
-            <div>인풋이 들어갈거야</div>
-
-
-            <button ref={topbarmenubuttonRef} type="button" className="p-link layout-topbar-menu-button layout-topbar-button" onClick={showProfileSidebar}>
-                <i className="pi pi-ellipsis-v" />
-            </button>
-
-            <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
-                <Link href="/auth/login">
-                    <span>당신의 공간을 에어비앤비하세요</span>
-                </Link>
-                <button type="button" className="p-link layout-topbar-button">
-                    <i className="pi pi-calendar"></i>
-                    <span>Calendar</span>
-                </button>
-                <Link href="/documentation">
-                    <button type="button" className="p-link layout-topbar-button">
-                        <i className="pi pi-cog"></i>
-                        <span>Settings</span>
-                    </button>
-                </Link>
-            </div>
+  return (
+    <div className="layout-topbar">
+      <Link href="/">
+        <div className="layout-topbar-logo">
+          <>
+            <img
+              src={`${contextPath}/layout/images/logo-main.svg`}
+              width="177.22px"
+              height={"40px"}
+              widt={"true"}
+              alt="logo"
+            />
+            <span className="ir">pims</span>
+          </>
         </div>
-    );
+      </Link>
+      <div>인풋이 들어갈거야</div>
+
+      <button
+        ref={topbarmenubuttonRef}
+        type="button"
+        className="p-link layout-topbar-menu-button layout-topbar-button"
+        onClick={showProfileSidebar}
+      >
+        <i className="pi pi-ellipsis-v" />
+      </button>
+
+      <div
+        ref={topbarmenuRef}
+        className={classNames("layout-topbar-menu", {
+          "layout-topbar-menu-mobile-active": layoutState.profileSidebarVisible,
+        })}
+      >
+        <Link href="/auth/login">
+          <span>당신의 공간을 에어비앤비하세요</span>
+        </Link>
+        <button type="button" className="p-link layout-topbar-button">
+          <i className="pi pi-calendar"></i>
+          <span>Calendar</span>
+        </button>
+        <Link href="/documentation">
+          <button type="button" className="p-link layout-topbar-button">
+            <i className="pi pi-cog"></i>
+            <span>Settings</span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 });
 
 export default AppTopbar;
